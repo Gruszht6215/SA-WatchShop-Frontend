6251e3737c5ba47d1a1c41486ae1b9cafb42ab02
Gruszht6215 <kunkawee.s@ku.th>
2021-10-29 22:16:22 +0700

    update add item feature(upload img)

--- src/components/adminTable/addItem/AddItemForm.vue
+++ src/components/adminTable/addItem/AddItemForm.vue
@@ -8,7 +8,7 @@
       <b-field label="Item Name">
         <b-input
           type="name"
-          :value="itemForm.name"
+          v-model="itemForm.name"
           placeholder="item name"
           required
         >
@@ -18,7 +18,7 @@
       <b-field label="Price">
         <b-input
           type="price"
-          :value="itemForm.price"
+          v-model="itemForm.price"
           placeholder="item price"
           required
         >
@@ -28,7 +28,7 @@
       <b-field label="Remain">
         <b-input
           type="remain"
-          :value="itemForm.remain"
+          v-model="itemForm.remain"
           placeholder="item remain"
           required
         >
@@ -39,7 +39,7 @@
         <b-field label="Spare-Part">
           <b-input
             placeholder="Spare-Part"
-            :value="itemForm.part.join()"
+            v-model="itemForm.part.join()"
             disabled
           ></b-input>
         </b-field>
@@ -51,10 +51,13 @@
           @click="isComponentModalActive = true"
         />
       </div>
+
+      <img :src="previewImage" class="uploading-image" />
+      <input type="file" @change="onFileChanged" />
     </section>
     <footer class="modal-card-foot">
       <b-button label="Cancel" @click="$parent.close()" />
-      <b-button label="Submit" type="is-primary" />
+      <b-button label="Submit" type="is-primary" @click="onSubmit" />
     </footer>
 
     <b-modal
@@ -77,6 +80,7 @@
 </template>
 
 <script>
+import ItemApi from "@/store/ItemApi";
 import SparePartApi from "@/store/SparepartApi";
 import SparePartForm from "@/components/adminTable/addItem/SparePartForm";
 
@@ -92,9 +96,12 @@ export default {
         price: null,
         remain: null,
         part: [],
+        selectedPartId: [],
       },
       spare_parts: [],
       leftSpareParts: [],
+      selectedFile: null,
+      previewImage: null,
     };
   },
   created() {
@@ -108,6 +115,14 @@ export default {
     },
     setSeletedSparepart(selectedSparepart) {
       this.itemForm.part = selectedSparepart;
+      selectedSparepart.forEach((part_name) => {
+        this.leftSpareParts.forEach((element) => {
+          if (part_name === element.name) {
+            this.itemForm.selectedPartId.push(element.id);
+          }
+        });
+      });
+      console.log(this.itemForm.selectedPartId);
     },
     addLeftSpareParts() {
       this.spare_parts.forEach((element) => {
@@ -118,7 +133,32 @@ export default {
     },
     test() {
       console.log("Part", this.itemForm.part);
+      console.log("Part", this.itemForm.selectedPartId);
       console.log("sparepart", this.spare_parts);
+    },
+    onFileChanged(event) {
+      this.selectedFile = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(this.selectedFile);
+      reader.onload = (e) => {
+        this.previewImage = e.target.result;
+        // console.log(this.previewImage);
+      };
+    },
+    async onSubmit() {
+      const formData = new FormData();
+      formData.append("files", this.selectedFile);
+
+      let payload = {
+        name: this.itemForm.name,
+        price: this.itemForm.price,
+        picture: formData,
+        remain: this.itemForm.remain,
+        spare_parts: this.itemForm.selectedPartId,
+      };
+      await ItemApi.dispatch("addItem", payload);
+
+      
     },
   },
 };
--- src/store/ItemApi.js
+++ src/store/ItemApi.js
@@ -8,39 +8,58 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-      data: [],
+    data: [],
   },
-  getters:{
+  getters: {
     items: (state) => state.data,
   },
   mutations: {
-    fetch(state, { res }){
-        state.data = res.data
+    fetch(state, { res }) {
+      state.data = res.data
     },
-    add(state, payload){
+    add(state, payload) {
       state.data.push(payload)
     }
   },
   actions: {
-    async fetchItems({ commit }){
-      let res= await Axios.get(api_endpoint + "/items")
+    async fetchItems({ commit }) {
+      let res = await Axios.get(api_endpoint + "/items")
       commit('fetch', { res })
       console.log("Fetch items API");
     },
-    async addItem({commit},payload){
-      let url = api_endpoint+"/items"
-      let body = {
-        name: payload.name,
-        price: payload.price,
-        status: payload.status
+
+    async addItem({ commit }, payload) {
+      let url = api_endpoint + "/items"
+      let upload_url = api_endpoint + '/upload'
+      console.log(payload.picture)
+      let res_upload = await Axios.post(upload_url, payload.picture)
+      if (res_upload.status === 200) {
+        const imageId = res_upload.data[0].id;
+
+        let body = {
+          name: payload.name,
+          price: payload.price,
+          status: payload.status,
+          picture: imageId,
+          remain: payload.remain,
+          spare_parts: payload.spare_parts,
+        }
+        let res = await Axios.post(url, body)
+        if (res.status === 200) {
+          commit('add', res.data)
+          swal("Success!", "Item has been added to your storage", "success")
+        } else {
+          //items error
+          console.error(res)
+        }
+      } else {
+        //upload pic error
+        console.error(res)
       }
-      let res = await Axios.post(url,body)
-      console.log("Add item");
-      console.log(res.data)
-      commit("add", res.data)
     },
-    async editItem({commit}, payload){
-      let url = api_endpoint+"/items/"+(payload.id)
+
+    async editItem({ commit }, payload) {
+      let url = api_endpoint + "/items/" + (payload.id)
       console.log(url);
       let body = {
         name: payload.name,
@@ -52,12 +71,13 @@ export default new Vuex.Store({
       console.log("Edit item");
       console.log(res.data)
     },
-    async deleteItem({commit},payload){
-      let url = api_endpoint+"/items/"+(payload.id)
+
+    async deleteItem({ commit }, payload) {
+      let url = api_endpoint + "/items/" + (payload.id)
       let res = await Axios.delete(url)
       console.log("Delete item");
       console.log(res.data)
-      if(res.status == 200){
+      if (res.status == 200) {
         return {
           success: true
         }
@@ -66,9 +86,10 @@ export default new Vuex.Store({
         success: false
       }
     },
-    async fetchItemById({commit}, payload){
-      let res = await Axios.get(api_endpoint+"/items?"+"id_in="+payload)
-      commit('fetch', {res})
+
+    async fetchItemById({ commit }, payload) {
+      let res = await Axios.get(api_endpoint + "/items?" + "id_in=" + payload)
+      commit('fetch', { res })
       console.log("Fetch Item By ID API");
       return res.data[0]
     }
